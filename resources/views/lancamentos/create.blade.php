ab8aba3eb89c651ce45ad80622a53ae72e690416
Matheus Sholl <m.sholl@hotmail.com>
2022-12-14 12:25:29 -0300

    adicionando o campo valor no form de criacao de lancamento

--- resources/views/lancamentos/create.blade.php
+++ resources/views/lancamentos/create.blade.php
@@ -1,13 +1,17 @@
 <x-layout title="Novo Lançamento">
-    <form class="row g-3" action="" method="post">
+    <form class="row g-3" action="/lancamentos/salvar" method="post">
         @csrf
         <div class="col-12">
-            <label for="tipo-lancamento" class="form-label">Tipo do lançamento</label>
-            <input type="text" class="form-control" id="tipo-lancamento" placeholder="Cartão de crédito">
+            <label for="nome-lancamento" class="form-label">Nome do lançamento</label>
+            <input type="text" class="form-control" id="nome-lancamento" placeholder="Cartão de crédito">
         </div>
-        <div class="col-12">
+        <div class="col-8">
             <label for="descricao" class="form-label">Descrição</label>
             <input type="text" class="form-control" id="descricao" placeholder="Pagamento cartão de crédito novembro/2022">
+        </div>
+        <div class="col-4">
+            <label for="valor" class="form-label">Valor</label>
+            <input type="text" class="form-control" id="valor" placeholder="R$">
         </div>
         <div class="col-12">
             <button type="submit" class="btn btn-dark">Cadastrar</button>
